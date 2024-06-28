91089d9f751aa83667809980ca9eac06aee3da85
Cyril Li <cyrils.li@icloud.com>
2024-06-27 23:58:34 -0500

    docs test

--- docs/conf.py
+++ docs/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'tpov'
+copyright = '2024, Cyril Li'
+author = 'Cyril Li'
 
 release = '0.1'
 version = '0.1.0'
