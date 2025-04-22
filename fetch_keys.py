eaaf66b2ffa81b658d085e7b05dca368aec0b4cb
Cyril Li <cyrils.li@icloud.com>
2025-04-21 21:07:58 -0500

    update code

--- fetch_keys.py
+++ fetch_keys.py
@@ -1,22 +1,32 @@
 # Built-in modules
-import re, threading, time
+import threading, time
 
 # Third-party modules
 from playwright.sync_api import sync_playwright
-import requests as r
-
-keys = {}
 
 def baidu_key ():
-    global keys
     with sync_playwright () as p:
         browser = p.chromium.launch ()
         page = browser.new_page ()
         page.goto ("https://maps.baidu.com", wait_until = "domcontentloaded")
         while True:
             if page.evaluate ("typeof SECKEY") != "undefined":
-                keys ["baidu"] = page.evaluate ("SECKEY")
-                if len (keys ["baidu"]) > 50: # Key is longer than 50 characters
+                key = page.evaluate ("SECKEY")
+                if len (key) > 50: # Key is longer than 50 characters
+                    print ("Baidu: " + key)
+                    return
+            time.sleep (0.1)
+
+def tdt_key ():
+    with sync_playwright () as p:
+        browser = p.chromium.launch ()
+        page = browser.new_page ()
+        page.goto ("https://www.tianditu.gov.cn", wait_until = "domcontentloaded")
+        while True:
+            if page.evaluate ('typeof document.getElementsByClassName("leaflet-tile-loaded")[0]') != "undefined":
+                key = page.evaluate ('new URLSearchParams(document.getElementsByClassName("leaflet-tile-loaded")[0].src).get("tk")')
+                if len (key) == 32: # Key is 32 characters
+                    print ("Tianditu: " + key)
                     return
             time.sleep (0.1)
 
@@ -25,10 +35,5 @@ with sync_playwright () as p:
     baidu = threading.Thread (target = baidu_key)
     baidu.start ()
     print ("Starting Tianditu thread...")
-    page = r.get ("https://www.tianditu.gov.cn/").text
-    keys ["tdt"] = re.search (r'(maptoken.+?")([0-9a-f]+)', page).group (2)
-    print ("Tianditu thread finished.")
-    baidu.join ()
-    print ("Baidu thread finished.")
-
-print (f"Baidu: {keys ['baidu']}\nTianditu: {keys ['tdt']}")
+    tdt = threading.Thread (target = tdt_key)
+    tdt.start ()
